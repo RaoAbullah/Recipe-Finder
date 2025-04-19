27af06a501e34700d30b9ebf45805704c1b536f7
Rao Abdullah Rajput <abdullahnasirrao7@gmail.com>
2025-04-19 11:45:33 +0500

    Initial commit

--- app.py
+++ app.py
@@ -1,34 +1,29 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 import requests
+
+from models import db, User, FavoriteRecipe  # Updated import
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask("RecipeFinder")
 app.config['SECRET_KEY'] = '12345'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
 API_KEY = 'd010db4503814e108c4e9b93e3248b74'
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
-
-class Favorite(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    recipe_id = db.Column(db.Integer)
-    title = db.Column(db.String(255))
-    image_url = db.Column(db.String(255))
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
     return render_template('index.html')
 
@@ -73,8 +68,7 @@ def search():
     query = request.form['query']
     url = f"https://api.spoonacular.com/recipes/complexSearch?query={query}&apiKey={API_KEY}&number=12"
     response = requests.get(url)
-    data = response.json()
-    recipes = data.get('results', [])
+    recipes = response.json().get('results', [])
     return render_template('search_results.html', recipes=recipes)
 
 @app.route('/suggest', methods=['POST'])
@@ -82,13 +76,13 @@ def suggest():
     ingredients = request.form['ingredients']
     url = f"https://api.spoonacular.com/recipes/findByIngredients?ingredients={ingredients}&apiKey={API_KEY}&number=12"
     response = requests.get(url)
-    data = response.json()
-    return render_template('results.html', recipes=data)
+    recipes = response.json()
+    return render_template('results.html', recipes=recipes)
 
 @app.route('/favorites')
 @login_required
 def view_favorites():
-    favorites = Favorite.query.filter_by(user_id=current_user.id).all()
+    favorites = FavoriteRecipe.query.filter_by(user_id=current_user.id).all()
     return render_template('favorites.html', favorites=favorites)
 
 @app.route('/save_favorite/<int:recipe_id>')
@@ -96,9 +90,9 @@ def view_favorites():
 def save_favorite(recipe_id):
     title = request.args.get('recipe_title')
     image_url = request.args.get('image_url')
-    existing = Favorite.query.filter_by(user_id=current_user.id, recipe_id=recipe_id).first()
+    existing = FavoriteRecipe.query.filter_by(user_id=current_user.id, recipe_id=recipe_id).first()
     if not existing:
-        new_fav = Favorite(user_id=current_user.id, recipe_id=recipe_id, title=title, image_url=image_url)
+        new_fav = FavoriteRecipe(user_id=current_user.id, recipe_id=recipe_id, recipe_title=title, image_url=image_url)
         db.session.add(new_fav)
         db.session.commit()
     return redirect(url_for('view_favorites'))
@@ -106,7 +100,7 @@ def save_favorite(recipe_id):
 @app.route('/remove_favorite/<int:recipe_id>', methods=['POST'])
 @login_required
 def remove_favorite(recipe_id):
-    favorite = Favorite.query.filter_by(user_id=current_user.id, recipe_id=recipe_id).first()
+    favorite = FavoriteRecipe.query.filter_by(user_id=current_user.id, recipe_id=recipe_id).first()
     if favorite:
         db.session.delete(favorite)
         db.session.commit()
