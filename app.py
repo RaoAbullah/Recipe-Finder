0418e47205a0bf7ca8d0bd2ba97b18c7d1ca09f4
Rao Abdullah <rtainstituteofeducation@gmail.com>
2025-04-19 13:25:32 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 import requests
-from models import db, User, FavoriteRecipe  # Updated import
+from models import db, User, FavoriteRecipe
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -11,7 +11,10 @@ app.config['SECRET_KEY'] = '12345'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Initialize and immediately create tables
 db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
@@ -21,12 +24,6 @@ API_KEY = 'd010db4503814e108c4e9b93e3248b74'
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-@app.before_first_request
-def create_tables():
-    # This ensures the tables are created before the first request is processed
-    with app.app_context():
-        db.create_all()
 
 @app.route('/')
 def index():
@@ -57,9 +54,8 @@ def login():
         if user:
             login_user(user)
             return redirect(url_for('index'))
-        else:
-            flash('Invalid credentials.', 'danger')
-            return redirect(url_for('login'))
+        flash('Invalid credentials.', 'danger')
+        return redirect(url_for('login'))
     return render_template('login.html')
 
 @app.route('/logout')
@@ -72,16 +68,14 @@ def logout():
 def search():
     query = request.form['query']
     url = f"https://api.spoonacular.com/recipes/complexSearch?query={query}&apiKey={API_KEY}&number=12"
-    response = requests.get(url)
-    recipes = response.json().get('results', [])
+    recipes = requests.get(url).json().get('results', [])
     return render_template('search_results.html', recipes=recipes)
 
 @app.route('/suggest', methods=['POST'])
 def suggest():
     ingredients = request.form['ingredients']
     url = f"https://api.spoonacular.com/recipes/findByIngredients?ingredients={ingredients}&apiKey={API_KEY}&number=12"
-    response = requests.get(url)
-    recipes = response.json()
+    recipes = requests.get(url).json()
     return render_template('results.html', recipes=recipes)
 
 @app.route('/favorites')
@@ -95,19 +89,23 @@ def view_favorites():
 def save_favorite(recipe_id):
     title = request.args.get('recipe_title')
     image_url = request.args.get('image_url')
-    existing = FavoriteRecipe.query.filter_by(user_id=current_user.id, recipe_id=recipe_id).first()
-    if not existing:
-        new_fav = FavoriteRecipe(user_id=current_user.id, recipe_id=recipe_id, recipe_title=title, image_url=image_url)
-        db.session.add(new_fav)
+    if not FavoriteRecipe.query.filter_by(user_id=current_user.id, recipe_id=recipe_id).first():
+        fav = FavoriteRecipe(
+            user_id=current_user.id,
+            recipe_id=recipe_id,
+            recipe_title=title,
+            image_url=image_url
+        )
+        db.session.add(fav)
         db.session.commit()
     return redirect(url_for('view_favorites'))
 
 @app.route('/remove_favorite/<int:recipe_id>', methods=['POST'])
 @login_required
 def remove_favorite(recipe_id):
-    favorite = FavoriteRecipe.query.filter_by(user_id=current_user.id, recipe_id=recipe_id).first()
-    if favorite:
-        db.session.delete(favorite)
+    fav = FavoriteRecipe.query.filter_by(user_id=current_user.id, recipe_id=recipe_id).first()
+    if fav:
+        db.session.delete(fav)
         db.session.commit()
     return redirect(url_for('view_favorites'))
 
